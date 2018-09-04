a542736b84f0f5787fccdf0eada1be2f53e967e8
aleclivinghouse <aleclivinghouse@yahoo.com>
2018-09-04 11:57:15 -0700

    first commit

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 'use strict';
 
-// Load array of notes
+const express = require('express');
 const data = require('./db/notes');
+const app = express();
 
 console.log('Hello Noteful!');
 
 // INSERT EXPRESS APP CODE HERE...
+app.get('/api/notes', (req, res) => {
+  res.json(data);
+});
+
+app.listen(8080, function () {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
