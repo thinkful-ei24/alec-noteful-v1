081d2c2589aea2031593acb138393111b3b13624
aleclivinghouse <aleclivinghouse@yahoo.com>
2018-09-05 12:20:15 -0700

    db-added

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 'use strict';
-
+const { PORT } = require('./config');
+const morgan = require('morgan');
+const {LOGGER} = require('./middleware/logger');
 const express = require('express');
 const app = express();
 const data = require('./db/notes');
+const simDB = require('./db/simDB');
+const notes = simDB.initialize(data);
 app.use(express.static('public'));
+app.use(morgan(LOGGER));
+
 
 //http://127.0.0.1:8080/api/notes/?searchTerm=life
 app.get('/api/notes', (req, res) => {
@@ -20,9 +26,14 @@ app.get('/api/notes/:id', (req, res) => {
   res.json(query);
 });
 
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  res.status(404).json({ message: 'Not Found' });
+});
 
 
-app.listen(8080, function () {
+app.listen(PORT, function () {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
   console.error(err);
